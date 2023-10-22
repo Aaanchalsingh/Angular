fb35e1ce8a9a7862bc04e05aec9490b752f0022e
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-10-22 08:43:22 +0530

    commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
+  template: '<p>good</p>',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
